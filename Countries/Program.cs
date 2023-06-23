bf4b5f000c113f8be3b56a933ba5c2cf5e159cb6
WaleriaMelnyk <vmeln3@softserveinc.com>
2023-06-24 02:04:06 +0300

    UC#1 Create endpoint

--- Countries/Program.cs
+++ Countries/Program.cs
@@ -16,6 +16,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+// Middleware for handling endpoints
+app.MapGet("Countries", async (IHttpClientFactory clientFactory, HttpContext httpContext) =>
+{
+    // Read the query parameters (optional)
+    var param1 = httpContext.Request.Query["param1"].ToString();
+    var param2 = httpContext.Request.Query["param2"].ToString();
+    var param3 = httpContext.Request.Query["param3"].ToString();
+
+    // Make a request to the REST Countries API
+    var client = clientFactory.CreateClient();
+    var response = await client.GetAsync("https://restcountries.com/v3.1/all");
+
+    if (response.IsSuccessStatusCode)
+    {
+        var content = await response.Content.ReadAsStringAsync();
+
+        // Response in JSON format
+        httpContext.Response.ContentType = "application/json";
+        await httpContext.Response.WriteAsync(content);
+    }
+    else
+    {
+        httpContext.Response.StatusCode = 500;
+        await httpContext.Response.WriteAsync("Error retrieving countries data");
+    }
+});
+
+app.Run();
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
